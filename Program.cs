3552427904effc519ae919a218b87f1dcffda463
Kenny <kenny-williamsStockd@live.nmit.ac.nz>
2022-01-10 11:50:11 +1300

    added variable and concatenation short story

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@ namespace CSharp_tutorial
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            // making a simple drawing
             Console.WriteLine("Hello World!");
             Console.WriteLine("     /|");
             Console.WriteLine("    / |");
@@ -14,6 +15,19 @@ namespace CSharp_tutorial
             Console.WriteLine(" /    |");
             Console.WriteLine("/_ _ _|");
             Console.ReadLine();
+            
+            //Variables
+            //with small story
+            //concatenating 
+            string characterName = "Gary"; // string data type in C# for variable
+            int characterAge = 38; // number data type or integer in C# for variable
+            Console.WriteLine("There was a man named " + characterName); //concatenating with characterName variable to add on to console print out
+            Console.WriteLine("He was "+ characterAge +" years old"); //concatenating with characterAge variable to add in the middle of the console print out
+            Console.WriteLine("He really liked the name " + characterName);
+            Console.WriteLine("But didn't like being " + characterAge);
+            Console.ReadLine();
+
+
         }
     }
 }

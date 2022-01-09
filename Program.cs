0248440691ca6f247f201b481ceca100009c7d11
Kenny <kenny-williamsStockd@live.nmit.ac.nz>
2022-01-10 10:43:21 +1300

    Added set up files for c#sharp tutorial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
